526610a5d1c8b2c952f90911fb84b0021956f672
Krzysztof Pniak <krzysztof.pniak@gmail.com>
2022-10-18 14:35:06 +0200

    fix(tailwind): fixes css examples

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     './docs/**/*.{js,jsx,ts,tsx}',
-    './examples/**/*.{js,jsx,ts,tsx}',
+    './../examples/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
